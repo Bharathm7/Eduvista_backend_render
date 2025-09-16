5c07b4ab408d51c184ad48f3209385a002da0eca
Bharathm7 <bharathmahesh69@gmail.com>
2025-09-16 12:25:40 +0530

    commit_auth

--- reports/urls.py
+++ reports/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     path("marks/", views.marks_list),
     path("report/<int:student_id>/", views.report_card), 
     path("report/pdf/<int:student_id>/", views.report_card_pdf, name="report_card_pdf"),
+    path("api/login/", views.supabase_login_api, name="api_login"),
+    path("api/signup/", views.supabase_signup_api, name="api_signup"),
+    path("api/home/", views.home_api, name="api_home"),
+    path("api/logout/", views.logout_api, name="api_logout"),
     
 
 ]
