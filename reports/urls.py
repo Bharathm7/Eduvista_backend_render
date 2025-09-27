837c372ea97f39d0915744a17f3854998a69f373
Somesh7723 <someshkolluru723@gmail.com>
2025-09-27 23:21:38 +0530

    updated urls.py

--- reports/urls.py
+++ reports/urls.py
@@ -13,14 +13,14 @@ urlpatterns = [
     path("classes/<str:teacher_id>/<str:subject_id>", views.class_list),
     path("subjects/<str:teacher_id>/", views.subject_list),
     path("subject/<str:teacher_id>/<int:class_id>", views.getSubject),
-    path("api/myclass/students/<int:my_class_id>/", views.students_of_my_class, name="students_of_my_class_with_id"),
+    path("myclass/students/<int:my_class_id>/", views.students_of_my_class, name="students_of_my_class_with_id"),
     path("mark_attendance/", views.mark_attendance),
     path("report/<int:student_id>/", views.report_card), 
     path("report/pdf/<int:student_id>/", views.report_card_pdf, name="report_card_pdf"),
-    path("api/login/", views.supabase_login_api, name="api_login"),
-    path("api/signup/", views.supabase_signup_api, name="api_signup"),
-    path("api/home/", views.home_api, name="api_home"),
-    path("api/logout/", views.logout_api, name="api_logout"),
+    path("login/", views.supabase_login_api, name="api_login"),
+    path("signup/", views.supabase_signup_api, name="api_signup"),
+    path("home/", views.home_api, name="api_home"),
+    path("logout/", views.logout_api, name="api_logout"),
     
 
 ]
